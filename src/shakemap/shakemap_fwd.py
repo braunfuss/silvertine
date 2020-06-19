c9eac637def38e42c1c0e8a26329b60c36e19b80
braunfuss <asteinbe@gfz-potsdam.de>
2020-06-19 19:13:52 +0200

    bayesian shakemap

--- src/shakemap/shakemap_fwd.py
+++ src/shakemap/shakemap_fwd.py
@@ -9,46 +9,80 @@ from pyrocko import gf, trace, plot, beachball, util, orthodrome
 from pyrocko import moment_tensor as pmt
 import _pickle as pickle
 from mpl_toolkits.basemap import Basemap
+import copy
 
 km = 1000.
 
 util.setup_logging('gf_shakemap')
 
 
-def make_shakemap(engine, source, store_id, folder, stations=None, save=True):
+def make_shakemap(engine, source, store_id, folder, stations=None, save=True,
+                  stations_corrections_file=None, pertub_mechanism=True,
+                  pertub_degree=20, measured=None):
     targets, norths, easts, stf_spec = get_scenario(engine,
                                                     source,
                                                     store_id)
-    response = engine.process(source, targets)
-    values = post_process(response, norths, easts, stf_spec, savedir=folder,
-                          save=save)
+
+    if pertub_mechanism is True:
+        sources = []
+        for i in range(0, 2):
+            source_pert = copy.deepcopy(source)
+            mts = source_pert.pyrocko_moment_tensor()
+            strike, dip, rake = mts.both_strike_dip_rake()[0]
+            strike = num.random.uniform(strike-pertub_degree,
+                                        strike+pertub_degree)
+            dip = num.random.uniform(dip-pertub_degree,
+                                     dip+pertub_degree)
+            rake = num.random.uniform(rake-pertub_degree,
+                                      rake+pertub_degree)
+            mtm = pmt.MomentTensor.from_values((strike, dip, rake))
+            source_pert.mnn = mtm.mnn
+            source_pert.mee = mtm.mee
+            source_pert.mdd = mtm.mdd
+            source_pert.mne = mtm.mne
+            source_pert.mnd = mtm.mnd
+            source_pert.med = mtm.med
+            sources.append(source_pert)
+    else:
+        sources = [source]
+    values_pertubed = []
+    values_stations_pertubed = []
+    for source in sources:
+        response = engine.process(source, targets)
+        values = post_process(response, norths, easts, stf_spec, savedir=folder,
+                              save=save)
+        values_pertubed.append(values)
+        if stations is not None:
+            targets_stations, norths_stations, easts_stations, stf_spec = get_scenario(engine,
+                                                                                       source,
+                                                                                       store_id,
+                                                                                       stations=stations)
+            response_stations = engine.process(source, targets_stations)
+            values_stations = post_process(response_stations, norths_stations,
+                                           easts_stations,
+                                           stf_spec, stations=True,
+                                           savedir=folder, save=save)
+            values_stations = values_stations[0][0:len(stations)]
+            if stations_corrections_file is not None:
+                stations_corrections_file = num.loadtxt(stations_corrections_file)
+                stations_corrections_value = []
+                for st in stations:
+                    for stc in stations_corrections_file:
+                        if st.station == stc[0]:
+                            stations_corrections_value.append(stc[1])
+                values_stations = values_stations * num.asarray(stations_corrections_value)
+            values_stations_pertubed.append(values_stations)
     if stations is not None:
-        targets_stations, norths_stations, easts_stations, stf_spec = get_scenario(engine,
-                                                                                   source,
-                                                                                   store_id,
-                                                                                   stations=stations)
-        response_stations = engine.process(source, targets_stations)
-        values_stations = post_process(response_stations, norths_stations,
-                                       easts_stations,
-                                       stf_spec, stations=True,
-                                       savedir=folder, save=save)
-        values_stations = values_stations[0][0:len(stations)]
-        if stations_corrections_file is not None:
-            stations_corrections_file = num.loadtxt(stations_corrections_file)
-            stations_corrections_value = []
-            for st in stations:
-                for stc in stations_corrections_file:
-                    if st.station == stc[0]:
-                        stations_corrections_value.append(stc[1])
-            values_stations = values_stations * num.asarray(stations_corrections_value)
-        plot_shakemap(source, norths, easts, values, 'gf_shakemap.png', folder,
+        plot_shakemap(sources, norths, easts, values_pertubed,
+                      'gf_shakemap.png', folder,
                       stations,
-                      values_stations=values_stations,
+                      values_stations_list=values_stations_pertubed,
                       norths_stations=norths_stations,
                       easts_stations=easts_stations)
 
     else:
-        plot_shakemap(source, norths, easts, values, 'gf_shakemap.png', folder,
+        plot_shakemap(sources, norths, easts, values_pertubed,
+                      'gf_shakemap.png', folder,
                       stations)
 
 
@@ -180,14 +214,29 @@ def load_shakemap(path):
     return values, easts, norths
 
 
-def plot_shakemap(source, norths, easts, values, filename, folder, stations,
-                  values_stations=None, easts_stations=None,
+def plot_shakemap(sources, norths, easts, values_list, filename, folder,
+                  stations,
+                  values_stations_list=None, easts_stations=None,
                   norths_stations=None, latlon=True, show=False,
                   plot_background_map=True):
     plot.mpl_init()
     fig = plt.figure(figsize=plot.mpl_papersize('a5', 'landscape'))
     axes = fig.add_subplot(1, 1, 1, aspect=1.0)
-    mt = source.pyrocko_moment_tensor()
+    mts = []
+    plot_kwargs = {
+        'size': 4000,
+        'edgecolor': 'black'
+        }
+
+    for i, source in enumerate(sources):
+        mts.append(source.pyrocko_moment_tensor())
+        if i == 0:
+            best_mt = source.pyrocko_moment_tensor()
+    for i, values_pertubed in enumerate(values_list):
+        if i == 0:
+            values = values_pertubed
+    if values_stations_list is not None:
+        values_stations = values_stations_list[0]
     _, vmax = num.min(values), num.max(values)
 
     if latlon is False:
@@ -204,12 +253,11 @@ def plot_shakemap(source, norths, easts, values, filename, folder, stations,
 
         fig.colorbar(im, label='Acceleration [m/s^2]')
 
-        beachball.plot_beachball_mpl(
-            mt, axes,
-            position=(0., 0.),
+        beachball.plot_fuzzy_beachball_mpl_pixmap(
+            mts, axes, best_mt,
+            position=(0, 0.),
             color_t='black',
-            zorder=2,
-            size=20.)
+            **plot_kwargs)
         if values_stations is not None:
             plt.scatter(easts_stations/km, norths_stations/km,
                         c=values_stations, s=36, cmap=plt.get_cmap('YlOrBr'),
@@ -222,45 +270,67 @@ def plot_shakemap(source, norths, easts, values, filename, folder, stations,
                                                north, east)
             lats.append(lat)
             lons.append(lon)
+
+        if plot_background_map is True:
+            map = Basemap(projection='merc',
+                          llcrnrlon=num.min(lons),
+                          llcrnrlat=num.min(lats),
+                          urcrnrlon=num.max(lons),
+                          urcrnrlat=num.max(lats),
+                          resolution='h', epsg=3395)
+            ratio_lat = num.max(lats)/num.min(lats)
+            ratio_lon = num.max(lons)/num.min(lons)
+
+            map.drawmapscale(num.min(lons)+ratio_lon*0.25, num.min(lats)+ratio_lat*0.25, num.mean(lons), num.mean(lats), 10)
+            parallels = num.arange(num.around(num.min(lats), decimals=2), num.around(num.max(lats), decimals=2), 0.1)
+            meridians = num.arange(num.around(num.min(lons), decimals=2), num.around(num.max(lons), decimals=2), 0.1)
+            map.drawparallels(parallels, labels=[1, 0,0,0], fontsize=22)
+            map.drawmeridians(meridians, labels=[1, 1,0,1], fontsize=22)
+            xpixels = 1000
+            map.arcgisimage(service='World_Shaded_Relief', xpixels=xpixels,
+                            verbose=False, zorder=1, cmap="gray")
+
     #    axes.set_xlim(lats.min()/km, lats.max()/km)
     #    axes.set_ylim(norths.min()/km, norths.max()/km)
-        beachball.plot_beachball_mpl(
-            mt, axes,
-            position=(source.lat, source.lon),
+        if plot_background_map is True:
+            pos1, pos2 = map(source.lon, source.lat)
+        else:
+            pos1 = source.lat
+            pos2 = source.lon
+        beachball.plot_fuzzy_beachball_mpl_pixmap(
+            mts, axes, best_mt,
+            position=(pos1, pos2),
             color_t='black',
             zorder=2,
-            size=20.)
+            **plot_kwargs)
 
+        if plot_background_map is True:
+            lats_map, lons_map = map(lons, lats)
+            values[values == 0] = 'nan'
+            alpha=0.5
+        else:
+            lats_map, lons_map = lats, lons
+            alpha=1.
         im = axes.contourf(
-            lats, lons, values.T,
+            lats_map, lons_map, values.T,
             vmin=0., vmax=vmax,
-            cmap=plt.get_cmap('YlOrBr'))
+            cmap=plt.get_cmap('YlOrBr'),
+            alpha=alpha)
 
         fig.colorbar(im, label='Acceleration [m/s^2]')
-        if values_stations is not None:
+        if values_stations_list is not None:
             st_lats, st_lons = [], []
             for st in stations:
                 st_lats.append(st.lat)
                 st_lons.append(st.lon)
-            plt.scatter(st_lats, st_lons,
+            if plot_background_map is True:
+                st_lats, st_lons = map(st_lons, st_lats)
+            plt.scatter(st_lons, st_lats,
                         c=values_stations, s=36, cmap=plt.get_cmap('YlOrBr'),
-                        vmin=0., vmax=vmax, edgecolor="k")
-
-    if plot_background_map is True:
-        map = Basemap(projection='merc',
-                      llcrnrlon=num.min(lons),
-                      llcrnrlat=num.min(lats),
-                      urcrnrlon=num.max(lons),
-                      urcrnrlat=num.max(lats),
-                      resolution='h', epsg=3395)
-        ratio_lat = num.max(lats)/num.min(lats)
-        ratio_lon = num.max(lons)/num.min(lons)
-
-        map.drawmapscale(num.min(eastings)+ratio_lon*0.25, num.min(northings)+ratio_lat*0.25, num.mean(eastings), num.mean(northings), 30)
-
-        xpixels = 1000
-        map.arcgisimage(service='World_Shaded_Relief', xpixels=xpixels,
-                        verbose=False)
+                        vmin=0., vmax=vmax, edgecolor="k", alpha=alpha)
+            for k, st in enumerate(stations):
+                plt.text(st_lons[k], st_lats[k], str(st.station))
+        axes.contour(lats_map, lons_map, values.T, cmap='brg', levels=[num.std(values.T)])
 
     fig.savefig(folder+filename)
     if show is True:
